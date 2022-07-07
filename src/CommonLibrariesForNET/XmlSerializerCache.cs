565723a3e1499b271f04caac222cb5e116295e67
EagleX18 <dries.vanbeveren@gmail.com>
2022-07-07 22:34:57 +0200

    XmlSerializer caching

---
+++ src/CommonLibrariesForNET/XmlSerializerCache.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Salesforce.Common
+{
+    public class XmlSerializerCache
+    {
+        public static Lazy<XmlSerializerCache> Instance = new Lazy<XmlSerializerCache>(() => new XmlSerializerCache(), true);
+
+        private XmlSerializerCache()
+        {
+            XmlSerializerDictionary = new ConcurrentDictionary<string, XmlSerializer>();
+        }
+
+        public ConcurrentDictionary<string, XmlSerializer> XmlSerializerDictionary { get; }
+    }
+}
