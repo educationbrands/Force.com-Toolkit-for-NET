33daf9b90456d0bd1ff6319ec39a70d4c69025f7
holoncom <github.rsiera@holoncom.eu>
2022-12-19 05:33:07 +0100

    upgrade Nugets

--- src/CommonLibrariesForNET/XmlHttpClient.cs
+++ src/CommonLibrariesForNET/XmlHttpClient.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Xml;
-using System.Xml.Linq;
 using Salesforce.Common.Internals;
 using Salesforce.Common.Models.Json;
 
